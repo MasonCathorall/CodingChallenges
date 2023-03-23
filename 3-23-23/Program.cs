1e89efc0fddd06b436a2b7290b4a9910c9eb619d
MasonCathorall <masoncathorallrev@gmail.com>
2023-03-23 10:57:11 -0400

    Finish challenge

---
+++ 3-23-23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodingChallenges
+{
+    class Program
+    {
+        public static int CircuitPower(int a, int b) => a*b;
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing 230 and 10: Expect 2300");
+            Console.WriteLine(CircuitPower(230, 10));
+            Console.WriteLine();
+
+            Console.WriteLine("Testing 110 and 3: Expect 330");
+            Console.WriteLine(CircuitPower(110, 3));
+            Console.WriteLine();
+
+            Console.WriteLine("Testing 480 and 20: Expect 9600");
+            Console.WriteLine(CircuitPower(480, 20));
+        }
+    }
+}
