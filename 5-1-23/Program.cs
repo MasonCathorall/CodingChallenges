b6325c97beede518261673b6b5acddb724a52ef9
MasonCathorall <masoncathorallrev@gmail.com>
2023-05-01 16:07:25 -0400

    new challenge

---
+++ 5-1-23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodingChallenges
+{
+    public class Program1
+    {
+        public static void Main(string[] args)
+        {
+            int rounds = 0;
+            Console.Write("Please enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            while(num != 1)
+            { 
+                if(num % 2 == 0)
+                {
+                    rounds++;
+                    num = num / 2;
+                } 
+                else
+                {
+                    rounds++;
+                    num = (num * 3) + 1;
+                }
+            }
+
+            Console.WriteLine(rounds.ToString());
+        }
+    }
+}
